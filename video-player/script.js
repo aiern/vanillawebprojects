ad99392b8651d45cfcecbf8644e2ab26ce61e2f6
ai_ern <im_in@outlook.sg>
2020-10-26 22:58:01 +0800

    initial stucture and layout

---
+++ video-player/script.js
@@ -0,0 +1,42 @@
+const video = document.getElementById('video')
+const play = document.getElementById('play')
+const stop = document.getElementById('stop')
+const progress = document.getElementById('progress')
+const timestamp = document.getElementById('timestamp')
+
+// Play & pause video 
+function toggleVideoStatus() {
+    return true;
+}
+
+// Update the play/pause icon
+function updatePlayIcon () {
+    return true;
+}
+
+// Update progress & timestamp 
+function updateProgress() {
+    return true;
+}
+
+// Set video time to progress
+function setVideoProgress() {
+    return true;
+}
+
+// Stop Video
+function stopVideo() {
+    return true;
+}
+
+// Event listeners
+video.addEventListener ('click', toggleVideoStatus);
+video.addEventListener ('pause', updatePlayIcon);
+video.addEventListener ('play', updatePlayIcon);
+video.addEventListener ('timeupdate', updateProgress);
+
+play.addEventListener('click', toggleVideoStatus);
+stop.addEventListener('click', stopVideo);
+progress.addEventListener('change', setVideoProgress);
+
+
